2cad1a2f6757d6b781e99805b45421206de313dc
willwellg <18811409120@163.com>
2019-02-21 17:40:02 +0800

    添加一行注释

--- Django_web/models.py
+++ Django_web/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django import forms
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length = 20)
@@ -7,4 +7,8 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+class FileUploadForm(forms.Form):
+    name = forms.CharField(max_length=20, min_length=2, required=True, label="名称：")
+    my_file = forms.CharField(label="文件名称")
 
--- Django_web/views.py
+++ Django_web/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from .models import Article
+from .models import *
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+import xlrd
 
 # Create your views here.
 def index(request):
@@ -40,3 +41,38 @@ def updateByID(requst):
     i = requst.GET['id']
     b = Article.objects.get(id = str(i))
     return render_to_response('base/update.html', {'data': b})
+
+def uploade(request):
+    if request == 'POST':
+        myform = FileUploadForm(request.POST, request.FILES)
+
+        if myform.is_valid():
+            f = request.FILES
+
+            wb = xlrd.open_workbook(filename=None, file_content=f.read())
+            table = wb.sheet()[0]
+            nrow = table.nrows
+            ncol = table.ncols
+            print(nrow, ncol)
+
+            for i in range(1, nrow+1):
+                row_value = table.row_value(i)
+                st = Article()
+                id = row_value.rowvalue[1]
+                title = row_value.rowvalue[2]
+                author = row_value.rowvalue[3]
+                st.title = title
+                st.author = author
+                st.save()
+    return HttpResponseRedirect("http://127.0.0.1:8000/query")
+
+
+def upload_excel(request):
+
+    return render_to_response('base/uplaod_excel.html')
+
+
+
+
+
+
