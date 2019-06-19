060f2eee610288118205cb84fa28638fdff570fe
willwellg <18811409120@163.com>
2019-06-19 10:21:12 +0800

    (1)实现匹配文本高亮 (2)修改查询分页样式

--- apps/Django_web/views.py
+++ apps/Django_web/views.py
@@ -1,11 +1,12 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
+from django.conf import settings
 from .models import *
 
 # Create your views here.
 class ArticleListView(ListView):
     model = Article
-    paginate_by = 2
+    paginate_by = settings.HAYSTACK_SEARCH_RESULTS_PER_PAGE
 
     def get_queryset(self):
         article_list = Article.objects.all().order_by("-modify_time")
